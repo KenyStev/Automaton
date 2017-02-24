b1e4d5292296feb81f5354a6eb9e3b9921781605
kenystev <kjestevez.0795@gmail.com>
2017-02-24 13:36:25 -0600

    set operations automaton_gui (Union, Intersection, Difference, Complement)

--- js/setOperationsInterface.js
+++ js/setOperationsInterface.js
@@ -1,13 +1,31 @@
 function getSelectorsDfa0(){return document.getElementById('selecter-dfa0').children}
 function getSelectorsDfa1(){return document.getElementById('selecter-dfa1').children}
+function getOperation(){return document.getElementById('dfa-operation-title').innerHTML}
+function hideModals(node){
+	let parents = document.getElementsByClassName(node.getAttribute('data-dismiss'))
+	for(let parent of parents){
+		parent.style.display = 'none'
+	}
+}
+
+function updateSelectors(id){
+	let selector = document.getElementById(id)
+	selector.innerHTML = ''
+	let index = 0
+	for(let automaton of dfa_automatons){
+		let spanItem = document.createElement('span')
+		spanItem.className = 'selecter-item'
+		spanItem.setAttribute("data-value",index)
+		spanItem.innerHTML = automaton.name
+		selector.appendChild(spanItem)
+		index++
+	}
+}
 
 var cancelButton =  document.getElementsByClassName("modal-cancel")
 for(let b of cancelButton){
 	b.addEventListener("click", () => {
-		let parents = document.getElementsByClassName(b.getAttribute('data-dismiss'))
-		for(let parent of parents){
-			parent.style.display = 'none'
-		}
+		hideModals(b)
 	})
 }
 
@@ -15,8 +33,19 @@ var invoks = document.getElementsByClassName('model-invok')
 for(let invok of invoks){
 	let idModal = invok.getAttribute("href")
 	let modal = document.getElementById(idModal.substring(1,idModal.length))
+	let title = invok.innerHTML
+	let selecterDFA1 = document.getElementById('selecter-dfa1')
 	invok.addEventListener("click", () => {
-		console.log(idModal)
+		updateSelectors('selecter-dfa0')
+		updateSelectors('selecter-dfa1')
+		setListenerForSelectAutomaton(getSelectorsDfa0())
+		setListenerForSelectAutomaton(getSelectorsDfa1())
+		document.getElementById('dfa-operation-title').innerHTML = title
+		if (title=='Complement') {
+			selecterDFA1.style.display = 'none'
+		}else{
+			selecterDFA1.style.display = 'block'
+		}
 		modal.style.display = 'block'
 	})
 }
@@ -29,28 +58,41 @@ function setListenerForSelectAutomaton (selectedItems) {
 				s.className = 'selecter-item'
 				if (s.getAttribute('data-value')==data_value) s.className += ' selected'
 			}
-			
-			console.log("data_value")
-			console.log(data_value)
 		})
 	}
 }
-setListenerForSelectAutomaton(getSelectorsDfa0())
-setListenerForSelectAutomaton(getSelectorsDfa1())
 
-// var generators = document.getElementsByClassName('generate-operation')
-// for(let gen of generators){
-// 	let selected_0 = getSelectorsDfa0()
-// 	for(let s of selected_0){
-// 		if (s.className.) {
+var generators = document.getElementsByClassName('generate-operation')
+for(let gen of generators){
+	gen.addEventListener("click",() => {
+		let selected_0 = getSelectorsDfa0()
+		for(let s of selected_0){
+			if (s.className.indexOf('selected')!=-1) {
+				selected_0 = s.getAttribute('data-value')
+			}
+		}
+		let selected_1 = getSelectorsDfa1()
+		for(let s of selected_1){
+			if (s.className.indexOf('selected')!=-1) {
+				selected_1 = s.getAttribute('data-value')
+			}
+		}
+		let processed = processOperation(getOperation(),selected_0,selected_1)
+		network.setData(processed.toDataSet())
+		hideModals(gen)
+	})
+}
 
-// 		}
-// 	}
-
-// 	gen.addEventListener("click",() => {
-// 		console.log(selected_0)
-// 		console.log(dfa_automatons[selected_0])
-// 		console.log(selected_1)
-// 		console.log(dfa_automatons[selected_1])
-// 	})
-// }
+function processOperation(operation,index0,index1){
+	let processed = undefined
+	if (operation=='Complement') {
+		processed = AutomatonJS.complementAutomaton(dfa_automatons[index0])
+	}else if (operation=='Union'){
+		processed = AutomatonJS.unionAutomaton(dfa_automatons[index0],dfa_automatons[index1])
+	}else if (operation=='Intersection'){
+		processed = AutomatonJS.intersectionAutomaton(dfa_automatons[index0],dfa_automatons[index1])
+	}else if (operation=='Difference'){
+		processed = AutomatonJS.differenceAutomaton(dfa_automatons[index0],dfa_automatons[index1])
+	}
+	return processed
+}
